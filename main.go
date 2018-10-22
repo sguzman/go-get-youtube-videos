3c80ae791d957de3cc5e83864192f140ae300697
Salvador Guzman <guzmansalv@gmail.com>
2018-10-22 07:35:12 -0700

    getting video ids

--- main.go
+++ main.go
@@ -116,10 +116,109 @@ func mapJson(d *goquery.Document) interface{} {
     return jsonMap
 }
 
+func videoSerials(obj interface{}) {
+    m1, ok1 := obj.(map[string]interface{})
+    if !ok1 {
+        panic("Failed type cast")
+    }
+
+    m2, ok2 := m1["contents"].(map[string]interface{})
+    if !ok2 {
+        panic("Failed type cast")
+    }
+
+    m3, ok3 := m2["twoColumnBrowseResultsRenderer"].(map[string]interface{})
+    if !ok3 {
+        panic("Failed type cast")
+    }
+
+    m4, ok4 := m3["tabs"].([]interface{})
+    if !ok4 {
+        panic("Failed type cast")
+    }
+
+    m5, ok5 := m4[1].(map[string]interface{})
+    if !ok5 {
+        panic("Failed type cast")
+    }
+
+    m6, ok6 := m5["tabRenderer"].(map[string]interface{})
+    if !ok6 {
+        panic("Failed type cast")
+    }
+
+    m7, ok7 := m6["content"].(map[string]interface{})
+    if !ok7 {
+        panic("Failed type cast")
+    }
+
+    m8, ok8 := m7["sectionListRenderer"].(map[string]interface{})
+    if !ok8 {
+        panic("Failed type cast")
+    }
+
+    m9, ok9 := m8["contents"].([]interface{})
+    if !ok9 {
+        panic("Failed type cast")
+    }
+
+    m10, ok10 := m9[0].(map[string]interface{})
+    if !ok10 {
+        panic("Failed type cast")
+    }
+
+    m11, ok11 := m10["itemSectionRenderer"].(map[string]interface{})
+    if !ok11 {
+        panic("Failed type cast")
+    }
+
+    m12, ok12 := m11["contents"].([]interface{})
+    if !ok12 {
+        panic("Failed type cast")
+    }
+
+    m13, ok13 := m12[0].(map[string]interface{})
+    if !ok13 {
+        panic("Failed type cast")
+    }
+
+    m14, ok14 := m13["gridRenderer"].(map[string]interface{})
+    if !ok14 {
+        panic("Failed type cast")
+    }
+
+    m15, ok15 := m14["items"].([]interface{})
+    if !ok15 {
+        panic("Failed type cast")
+    }
+
+    serials := make([]string, len(m15))
+    for i := 0; i < len(m15); i++ {
+        item, ok := m15[i].(map[string]interface{})
+        if !ok {
+            panic("Failed type cast")
+        }
+
+        item1, ok16 := item["gridVideoRenderer"].(map[string]interface{})
+        if !ok16 {
+            panic("Failed type cast")
+        }
+
+        item2, ok17 := item1["videoId"].(string)
+        if !ok17 {
+            panic("Failed type cast")
+        }
+
+        serials[i] = item2
+        fmt.Println(item2)
+    }
+}
+
 func process() {
     channel := channels()
     d := doc(channel)
-    mapJson(d)
+    inter := mapJson(d)
+    videoSerials(inter)
 }
 
 func main() {
